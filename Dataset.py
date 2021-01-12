1b07bcd1394d867166a8da2e6c6af1636dfb1824
EmanueleMusumeci <emanuele.musumeci@outlook.com>
2021-01-12 15:30:32 +0100

    Dataset structure

--- Dataset.py
+++ Dataset.py
@@ -1,9 +1,34 @@
 import torch
 
-class UrbanSoundDataset(torch.utils.data.Dataset):
-    def __init__(self)
+class SoundDataset(torch.utils.data.Dataset):
+    def __init__(self, dataset_dir, dataset_name):
+        self.dataset_dir = dataset_dir
+        self.dataset_name = dataset_name
+
+#TODO MICHELE
+        self.data = self.load_dataset(dataset_dir)
+
+#TODO MICHELE
+    def __getitem__(self, index):
+        #Decidere come salvare dati -> estrarre sample
+        #sample = <qualcosa>
+        
+        preprocessed_sample = self.preprocess(sample)
+
+        #Considerare quali labels usare
+        return preprocessed_sample, labels 
+
+    def preprocess(self, sample):
+#TODO EMANUELE
+        prep_sample = sample
+        return prep_sample
     
-    
+    def __len__(self):
+#TODO MICHELE
+        pass
+
 if __name__ == "__main__":
-    dataset = UrbanSoundDataset()
+    DATASET_DIR = "data"
+    DATASET_NAME = "UrbanSound8K"
+    dataset = UrbanSoundDataset(DATASET_DIR, DATASET_NAME)
 
